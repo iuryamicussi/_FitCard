8269eed1aa111cc6ff4762a2faf7d179f5172475
iuryAmicussi <vapo_iury@hotmail.com>
2019-01-23 12:22:55 -0200

    Criado abstração para configurar tabelas pelo entity e interface comum e implementação com o ef da repository.

---
+++ FitCard.Repositorios.Comum.Entity/RepositorioGenericoEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCard.Repositorios.Comum.Entity
+{
+    public class RepositorioGenericoEntity<TEntidade, TChave> : IRepositorioGenerico<TEntidade, TChave>
+        where TEntidade : class
+    {
+        protected DbContext _contexto;
+
+        public RepositorioGenericoEntity(DbContext contexto)
+        {
+            _contexto = contexto;
+        }
+
+        public void Alterar(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Attach(entidade);
+            _contexto.Entry(entidade).State = EntityState.Modified;
+            _contexto.SaveChanges();
+        }
+
+        public void Excluir(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Attach(entidade);
+            _contexto.Entry(entidade).State = EntityState.Deleted;
+            _contexto.SaveChanges();
+        }
+
+        public void ExcluirPorId(TChave id)
+        {
+            TEntidade entidade = SelecionarPorId(id);
+            Excluir(entidade);
+        }
+
+        public void Inserir(TEntidade entidade)
+        {
+            _contexto.Set<TEntidade>().Add(entidade);
+            _contexto.SaveChanges();
+        }
+
+        public IList<TEntidade> Selecionar() => _contexto.Set<TEntidade>().ToList();
+
+        public IList<TEntidade> Selecionar(Func<TEntidade, bool> predicate) =>
+            _contexto.Set<TEntidade>().Where(predicate).ToList();
+
+        public TEntidade SelecionarPorId(TChave id) => _contexto.Set<TEntidade>().Find(id);
+    }
+}
---
+++ FitCard.Repositorios.Comum/IRepositorioGenerico.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitCard.Repositorios.Comum
+{
+    public interface IRepositorioGenerico<TEntidade,TChave>
+        where TEntidade : class
+    {
+        IList<TEntidade> Selecionar();
+        IList<TEntidade> Selecionar(Func<TEntidade, bool> predicate);
+        TEntidade SelecionarPorId(TChave id);
+        void Inserir(TEntidade entidade);
+        void Alterar(TEntidade entidade);
+        void Excluir(TEntidade entidade);
+        void ExcluirPorId(TChave id);
+    }
+}
